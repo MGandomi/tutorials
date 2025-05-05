22543cd6ee3db1416a0212003b7d567d79a6927a
MGandomi <mobinagandomi83@gmail.com>
2025-05-05 10:54:33 +0330

    [ADD] Add slot

--- awesome_owl/static/src/card/card.js
+++ awesome_owl/static/src/card/card.js
@@ -6,7 +6,12 @@ export class Card extends Component {
     static template = "awesome_owl.card";
     static props = {
         title: { type: String },
-        content: { type: String },
         isMarkup: { type: Boolean, optional: true },
+	     slots: {
+             type: Object,
+             shape: {
+                 default: true
+             },
+         }
     };
 }
