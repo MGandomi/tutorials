bb6c6d6c512bac9f1ce24b603f965a8fc846c8a1
MGandomi <mobinagandomi83@gmail.com>
2025-05-11 09:46:42 +0330

    [IMP] awesome_dashboard :  Lazy loading the dashboard

--- awesome_dashboard/static/src/dashboard/dashboard.js
+++ awesome_dashboard/static/src/dashboard/dashboard.js
@@ -40,5 +40,6 @@ class AwesomeDashboard extends Component {
 
 }
 
-registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
-registry.category('components').add('awesome_dashboard.PieChart', PieChart);
+// registry.category("actions").add("awesome_dashboard.dashboard", AwesomeDashboard);
+// registry.category('components').add('awesome_dashboard.PieChart', PieChart);
+registry.category("lazy_components").add("AwesomeDashboard", AwesomeDashboard);
