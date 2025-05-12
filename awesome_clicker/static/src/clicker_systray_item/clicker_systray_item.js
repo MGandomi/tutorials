89945dcd629a3372e795c32eae7f665fad05488e
MGandomi <mobinagandomi83@gmail.com>
2025-05-12 13:57:50 +0330

    [IMP] awesome_clicker : Humanize the displayed value

--- awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
+++ awesome_clicker/static/src/clicker_systray_item/clicker_systray_item.js
@@ -1,13 +1,17 @@
-import { Component } from "@odoo/owl";
+import {Component} from "@odoo/owl";
 import {registry} from "@web/core/registry";
 import {useService} from "@web/core/utils/hooks";
 
-import { useClicker } from "../clicker_hook";
+import {useClicker} from "../clicker_hook";
+import {ClickerValue} from "../clicker_value/clicker_value";
 
 
 export class ClickerSystray extends Component {
     static template = "awesome_clicker.ClickerSystray";
     static props = {};
+	static components = {ClickerValue};
+
+
 
     setup() {
 
