cc03064e87fc7bf1c99b076b805adf7e168163aa
MGandomi <mobinagandomi83@gmail.com>
2025-05-13 15:19:30 +0330

    [IMP] awesome_gallery : Solve the concurrency problem

--- awesome_gallery/static/src/gallery_controller.js
+++ awesome_gallery/static/src/gallery_controller.js
@@ -1,5 +1,6 @@
 import { Layout } from "@web/search/layout";
 import { useService } from "@web/core/utils/hooks";
+import { KeepLast } from "@web/core/utils/concurrency";
 import { Component, onWillStart, onWillUpdateProps, useState } from "@odoo/owl";
 import { standardViewProps } from "@web/views/standard_view_props";
 
@@ -14,28 +15,37 @@ export class GalleryController extends Component {
     setup() {
         this.orm = useService("orm");
         this.images = useState({ data: [] });
-        onWillStart(async () => {
-            const { records } = await this.loadImages(this.props.domain);
-            this.images.data = records;
-        });
+        this.keeplast = new KeepLast();
 
-        onWillUpdateProps(async (nextProps) => {
+        const loadAndSetImages = async (domain) => {
+            const { records } = await this.loadImages(domain);
+            this.images.data = records;
+        };
+
+        onWillStart(() => loadAndSetImages(this.props.domain));
+
+        onWillUpdateProps((nextProps) => {
             if (JSON.stringify(nextProps.domain) !== JSON.stringify(this.props.domain)) {
-                const { records } = await this.loadImages(nextProps.domain);
-                this.images.data = records;
+                return loadAndSetImages(nextProps.domain);
             }
         });
     }
 
     loadImages(domain) {
-        return this.orm.webSearchRead(this.props.resModel, domain, {
-            limit: this.props.archInfo.limit,
-            specification: {
-                [this.props.archInfo.imageField]: {},
-            },
-            context: {
-                bin_size: true,
-            }
-        });
+        if (!this.props.archInfo || !this.props.archInfo.imageField) {
+            return Promise.resolve({ records: [] });
+        }
+
+        return this.keeplast.add(
+            this.orm.webSearchRead(this.props.resModel, domain, {
+                limit: this.props.archInfo.limit,
+                specification: {
+                    [this.props.archInfo.imageField]: {},
+                },
+                context: {
+                    bin_size: true,
+                },
+            })
+        );
     }
 }
